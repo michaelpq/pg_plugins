22eb65a8b191f75dda6f1f5686dbc45f5088e8f1
Michael Paquier <michael@otacoo.com>
2015-04-14 21:18:30 +0900

    hook_utility: first commit
    This utility was used as a demonstration for hooks in a past blog post,
    but I never actually released it in a correct shape, so here it is.

---
+++ hook_utility/hook_utility.c
@@ -0,0 +1,120 @@
+/*-------------------------------------------------------------------------
+ *
+ * hook_utility.c
+ *		Use utility hook to restrict operations on a couple of DDL queries.
+ *
+ * Copyright (c) 2013-2015, Michael Paquier
+ * Copyright (c) 1996-2015, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *		hook_utility/hook_utility.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#include "postgres.h"
+#include "miscadmin.h"
+#include "tcop/utility.h"
+
+PG_MODULE_MAGIC;
+
+void _PG_init(void);
+void _PG_fini(void);
+
+/* Hardcoded values of parameters that interact with the hook */
+static char *hook_dbname = "postres";
+static char *hook_username = "postgres";
+static ProcessUtility_hook_type prev_utility_hook = NULL;
+
+static void dbrestrict_utility(Node *parsetree,
+							   const char *queryString,
+							   ProcessUtilityContext context,
+							   ParamListInfo params,
+							   DestReceiver *dest,
+							   char *completionTag);
+static void load_params(void);
+
+static
+void dbrestrict_utility(Node *parsetree,
+						const char *queryString,
+						ProcessUtilityContext context,
+						ParamListInfo params,
+						DestReceiver *dest,
+						char *completionTag)
+{
+	/* Do our custom process on drop database */
+	switch (nodeTag(parsetree))
+	{
+		case T_DropdbStmt:
+		{
+			DropdbStmt *stmt = (DropdbStmt *) parsetree;
+			char *username = GetUserNameFromId(GetUserId());
+
+			/*
+			 * Check that only the authorized superuser foo can
+			 * drop the database undroppable_foodb.
+			 */
+			if (strcmp(stmt->dbname, hook_dbname) == 0 &&
+				strcmp(username, hook_username) != 0)
+				ereport(ERROR,
+						(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+						 errmsg("Only super-superuser \"%s\" can drop database \"%s\"",
+								hook_username, hook_dbname)));
+			break;
+		}
+
+		default:
+			break;
+	}
+
+	/*
+	 * Fallback to normal process, be it the previous hook loaded
+	 * or the in-core code path if the previous hook does not exist.
+	 */
+	if (prev_utility_hook)
+		prev_utility_hook(parsetree, queryString,
+						  context, params,
+						  dest, completionTag);
+	else
+		standard_ProcessUtility(parsetree, queryString,
+								context, params,
+								dest, completionTag);
+}
+
+static
+void load_params(void)
+{
+	/* Name of database that cannot be dropped by other users */
+	DefineCustomStringVariable("hook_utility.dbname",
+							   "Database on which DROP DATABASE is forbidden",
+							   "Default value is \"postgres\".",
+							   &hook_dbname,
+							   "postgres",
+							   PGC_POSTMASTER,
+							   0, NULL, NULL, NULL);
+
+
+	/* Name of use that cannot dropped the undropable database */
+	DefineCustomStringVariable("hook_utility.username",
+							   "User name able to do DROP DATABASE on given dbname",
+							   "Default value is \"postgres\".",
+							   &hook_username,
+							   "postgres",
+							   PGC_POSTMASTER,
+							   0, NULL, NULL, NULL);
+
+}
+
+void
+_PG_init(void)
+{
+	prev_utility_hook = ProcessUtility_hook;
+	ProcessUtility_hook = dbrestrict_utility;
+
+	/* Load custom parameters */
+	load_params();
+}
+void
+_PG_fini(void)
+{
+	ProcessUtility_hook = prev_utility_hook;
+}
