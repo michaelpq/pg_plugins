803c831c4bb622d69a0f2b562cc57e80a6626f36
Michael Paquier <michael@otacoo.com>
2015-06-09 13:48:39 +0900

    pg_wal_blocks: Basic infrastructure for this new module
    Now what remains is to code the core part.

---
+++ pg_wal_blocks/pg_wal_blocks.c
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------------------------
+ *
+ * pg_wal_blocks.c
+ *		Tracker of relation blocks touched by WAL records
+ *
+ * Copyright (c) 1996-2015, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *		pg_wal_blocks/pg_wal_blocks.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres_fe.h"
+
+int
+main(int argc, char **argv)
+{
+	exit(0);
+}
