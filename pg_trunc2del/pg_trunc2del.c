310c6e30e90f1f01fb2b6fe493a62172e5ed7096
Michael Paquier <michael@otacoo.com>
2015-10-31 18:31:57 +0400

    pg_trunc2del: Initial commit
    pg_trunc2del aims at replacing on-the-fly TRUNCATE statements by an
    equivalent DELETE and executing it transparently to the user. This
    extension has a couple of limits relation to triggers and other
    TRUNCATE options like CASCADE but this seems enough for the purpose
    of demonstration.
    
    Original idea of this extension from Julien Rouhaud, though I didn't
    reuse his idea and relied on SPI to provide a transparent query run
    from the user prospective.

---
+++ pg_trunc2del/pg_trunc2del.c
@@ -0,0 +1,114 @@
+/*-------------------------------------------------------------------------
+ *
+ * pg_trunc2del.c
+ *		Extension that executes TRUNCATE statements as DELETE statements.
+ *
+ * Copyright (c) 1996-2015, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *		  pg_trunc2del/pg_trunc2del.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "executor/spi.h"
+#include "fmgr.h"
+#include "tcop/utility.h"
+#include "utils/builtins.h"
+#include "utils/snapmgr.h"
+
+PG_MODULE_MAGIC;
+
+/*--- Functions --- */
+
+void	_PG_init(void);
+void	_PG_fini(void);
+
+static ProcessUtility_hook_type prev_utility_hook = NULL;
+
+static void
+trunc2del(Node *parsetree,
+		  const char *queryString,
+		  ProcessUtilityContext context,
+		  ParamListInfo params,
+		  DestReceiver *dest,
+		  char *completionTag)
+{
+	/*
+	 * Do custom processing for TRUNCATE. Note that this is not aimed at
+	 * doing much for TRUNCATE CASCADE and triggers that should fire here.
+	 * This becomes even more a mess should a DELETE trigger be defined
+	 * on this relation.
+	 */
+	switch (nodeTag(parsetree))
+	{
+		case T_TruncateStmt:
+		{
+			TruncateStmt   *stmt = (TruncateStmt *) parsetree;
+			RangeVar	   *rv = (RangeVar *) linitial(stmt->relations);
+			int				ret;
+			StringInfoData	buf;
+			Relation		rel;
+
+			/*
+			 * Check existence of relation queried, this is important in case
+			 * of an unexistent relation to not let the user know of this
+			 * run switch. As we are faking a TRUNCATE, it is as well important
+			 * to take a exclusive lock on the relation operated on.
+			 */
+			rel = heap_openrv(rv, AccessExclusiveLock);
+
+			SPI_connect();
+			initStringInfo(&buf);
+
+			appendStringInfo(&buf, "DELETE FROM ");
+
+			if (rv->schemaname)
+				appendStringInfo(&buf,"%s.",
+								 quote_identifier(rv->schemaname));
+
+			appendStringInfo(&buf, "%s;", quote_identifier(rv->relname));
+
+			ret = SPI_execute(buf.data, false, 0);
+
+			if (ret != SPI_OK_DELETE)
+				elog(ERROR, "Error while executing TRUNCATE (really?)");
+
+			SPI_finish();
+
+			/* keep lock until the end of transaction */
+			heap_close(rel, NoLock);
+			return;
+		}
+		default:
+			break;
+	}
+
+	/*
+	 * Fallback to normal process, be it the previous hook loaded
+	 * or the in-core code path if the previous hook does not exist.
+	 */
+	if (prev_utility_hook)
+		(*prev_utility_hook) (parsetree, queryString,
+							  context, params,
+							  dest, completionTag);
+	 else
+		 standard_ProcessUtility(parsetree, queryString,
+								 context, params,
+								 dest, completionTag);
+}
+
+void
+_PG_init(void)
+{
+	prev_utility_hook = ProcessUtility_hook;
+	ProcessUtility_hook = trunc2del;
+}
+
+void
+_PG_fini(void)
+{
+    ProcessUtility_hook = prev_utility_hook;
+}
