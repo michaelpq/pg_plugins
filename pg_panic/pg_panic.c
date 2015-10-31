3cdfcf4bc79b4063df7536427780a8f7fd8467a1
Michael Paquier <michael@otacoo.com>
2015-10-31 19:10:48 +0400

    pg_panic: Initial commit
    pg_panic is an extension triggering PANIC, or a server-wide failure, or
    the worst possible level possible in a Postgres instance in a non
    deterministic way using a user-settable luck factor. This extension may be
    useful to emulate FS or OS-level failures that could happen when Postgres
    is running and check the robustness of an upper-level application layer.
    
    Original idea from Julien Rouhaud and Michael Paquier, with extra ideas
    from Nicolas Thauvin.

---
+++ pg_panic/pg_panic.c
@@ -0,0 +1,79 @@
+/*-------------------------------------------------------------------------
+ *
+ * pg_panic.c
+ *		Kick a random PANIC at server level to test robustness of an
+ *		installation.
+ *
+ * Copyright (c) 1996-2015, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *		  pg_panic/pg_panic.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+#include "fmgr.h"
+
+#include "optimizer/planner.h"
+#include "utils/guc.h"
+
+PG_MODULE_MAGIC;
+
+static double luck_factor = 0.001;
+
+planner_hook_type prev_planner_hook = NULL;
+
+void _PG_init(void);
+void _PG_fini(void);
+
+static PlannedStmt *
+panic_hook(Query *parse, int cursorOptions, ParamListInfo boundParams)
+{
+	PlannedStmt *result;
+
+	/* should we panic for this query? */
+	if (random() < luck_factor * MAX_RANDOM_VALUE)
+		elog(PANIC, "Jinx! Bad luck for today.");
+
+	if (prev_planner_hook)
+		result = (*prev_planner_hook) (parse, cursorOptions, boundParams);
+	else
+		result = standard_planner(parse, cursorOptions, boundParams);
+
+	return result;
+}
+
+static void
+pg_panic_load_params(void)
+{
+	DefineCustomRealVariable("pg_panic.luck_factor",
+							 "percentage of triggering PANIC when planning query",
+							 "Default of 0.001, range of values being [0..1]",
+							 &luck_factor,
+							 0.001,
+							 0.0,
+							 1.0,
+							 PGC_USERSET,
+							 0,
+							 NULL,
+							 NULL,
+							 NULL);
+}
+
+void
+_PG_init(void)
+{
+	pg_panic_load_params();
+
+	/* switch hooks */
+	prev_planner_hook = planner_hook;
+	planner_hook = panic_hook;
+}
+
+void
+_PG_fini(void)
+{
+	/* reinstall default hook */
+	planner_hook = prev_planner_hook;
+}
