0019638de1585568d81ad4a61adfc55a1234eb8b
Michael Paquier <michael@otacoo.com>
2015-11-08 23:24:15 +0900

    blackhole: add IMMUTABLE and STABLE functions
    This will be used for a future blog post related to extensions.

--- blackhole/blackhole--1.0.sql
+++ blackhole/blackhole--1.0.sql
@@ -3,8 +3,18 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION blackhole" to load this file. \quit
 
--- This is a blackhole
-CREATE FUNCTION blackhole()
+-- Those are the blackhole functions
+CREATE FUNCTION blackhole_volatile()
 RETURNS text
-AS 'MODULE_PATHNAME'
-LANGUAGE C;
+AS 'MODULE_PATHNAME','blackhole'
+LANGUAGE C VOLATILE;
+
+CREATE FUNCTION blackhole_stable()
+RETURNS text
+AS 'MODULE_PATHNAME','blackhole'
+LANGUAGE C STABLE;
+
+CREATE FUNCTION blackhole_immutable()
+RETURNS text
+AS 'MODULE_PATHNAME','blackhole'
+LANGUAGE C IMMUTABLE;
