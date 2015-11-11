99b1cb17368c88cea1f9a3e71246e6adeb492643
Michael Paquier <michael@otacoo.com>
2015-11-11 11:43:51 +0900

    blackhole: Revert: "add IMMUTABLE and STABLE functions"
    This reverts commit 0019638, where it happens that this is actually not
    needed for the blog post in question. Let's keep this extension at its
    bare minimum.

--- blackhole/blackhole--1.0.sql
+++ blackhole/blackhole--1.0.sql
@@ -3,18 +3,8 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION blackhole" to load this file. \quit
 
--- Those are the blackhole functions
-CREATE FUNCTION blackhole_volatile()
+-- This is a blackhole
+CREATE FUNCTION blackhole()
 RETURNS text
-AS 'MODULE_PATHNAME','blackhole'
-LANGUAGE C VOLATILE;
-
-CREATE FUNCTION blackhole_stable()
-RETURNS text
-AS 'MODULE_PATHNAME','blackhole'
-LANGUAGE C STABLE;
-
-CREATE FUNCTION blackhole_immutable()
-RETURNS text
-AS 'MODULE_PATHNAME','blackhole'
-LANGUAGE C IMMUTABLE;
+AS 'MODULE_PATHNAME'
+LANGUAGE C;
