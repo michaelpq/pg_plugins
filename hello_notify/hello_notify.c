022c5741c9bc2463b58dd9007bc5cc8951943f35
Michael Paquier <michael@paquier.xyz>
2017-04-01 22:16:06 +0900

    Remove references to PG_VERSION_NUM in bgworker modules
    Those modules will not be able to work with older versions than
    Postgres 10 if left as-is, so let's keep life simple.

--- hello_notify/hello_notify.c
+++ hello_notify/hello_notify.c
@@ -265,12 +265,6 @@ _PG_init(void)
 	worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
 	worker.bgw_restart_time = 10;
 	worker.bgw_main_arg = (Datum) 0;
-#if PG_VERSION_NUM >= 90400
-	/*
-	 * Notify PID is present since 9.4. If this is not initialized
-	 * a static background worker cannot start properly.
-	 */
 	worker.bgw_notify_pid = 0;
-#endif
 	RegisterBackgroundWorker(&worker);
 }
