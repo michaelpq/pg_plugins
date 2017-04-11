a4111375249aa4cf894ec2535287271f95529ea0
Michael Paquier <michael@paquier.xyz>
2017-04-11 14:04:49 +0900

    pg_sasl_prepare: Use upstream implementation of SASLprep
    No need to maintain twice the same code.

--- pg_sasl_prepare/pg_sasl_prepare--1.0.sql
+++ pg_sasl_prepare/pg_sasl_prepare--1.0.sql
@@ -4,27 +4,7 @@
 \echo Use "CREATE EXTENSION pg_sasl_prepare" to load this file. \quit
 
 -- This is a pg_sasl_prepare
-CREATE FUNCTION pg_sasl_prepare(_int4)
-RETURNS _int4
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
-
--- Conversion functions
-CREATE FUNCTION utf8_to_array(text)
-RETURNS _int4
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;
-
-CREATE FUNCTION array_to_utf8(_int4)
+CREATE FUNCTION pg_sasl_prepare(text)
 RETURNS text
 AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;
-
--- Conversion table fetch
-CREATE OR REPLACE FUNCTION utf8_conv_table(
-    OUT code int,
-    OUT class smallint,
-    OUT decomposition _int4)
-RETURNS SETOF record
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;
+LANGUAGE C STRICT;
