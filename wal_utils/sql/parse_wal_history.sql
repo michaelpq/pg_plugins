28dd6647148266a19895b4e9034136164512bd68
Michael Paquier <michael@paquier.xyz>
2017-08-30 21:38:15 +0900

    wal_utils: Add more error checks for parsing

--- wal_utils/sql/parse_wal_history.sql
+++ wal_utils/sql/parse_wal_history.sql
@@ -17,4 +17,22 @@ INSERT INTO history_data VALUES (
 7       0/249BEB38      no recovery target specified
 ');
 SELECT parse_wal_history(data) FROM history_data;
+
+-- Sanity checks with some failures
+-- Timeline number is not an integer.
+TRUNCATE history_data;
+INSERT INTO history_data VALUES ('not_integer_tli 0/249BEB38 string');
+SELECT parse_wal_history(data) FROM history_data;
+-- Not three fields
+TRUNCATE history_data;
+INSERT INTO history_data VALUES ('1 not_lsn');
+SELECT parse_wal_history(data) FROM history_data;
+-- Timelines need to be increasing
+TRUNCATE history_data;
+INSERT INTO history_data VALUES (
+'2       0/117BEB70      no recovery target specified
+1       0/09D4F390      no recovery target specified
+');
+SELECT parse_wal_history(data) FROM history_data;
+
 DROP TABLE history_data;
