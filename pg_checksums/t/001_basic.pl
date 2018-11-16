8ad20951ec89b826648f7e27fe15d031b4794bf9
Michael Paquier <michael@paquier.xyz>
2018-11-16 14:05:09 +0900

    pg_checksums: Improve test coverage
    This adds a couple of tests for various configurations and option
    compatibilities, and cases of corruption detected.

--- pg_checksums/t/001_basic.pl
+++ pg_checksums/t/001_basic.pl
@@ -1,8 +1,17 @@
 use strict;
 use warnings;
 use TestLib;
-use Test::More tests => 8;
+use Test::More tests => 13;
 
 program_help_ok('pg_checksums');
 program_version_ok('pg_checksums');
 program_options_handling_ok('pg_checksums');
+
+command_fails([ 'pg_checksums', '-A', 'incorrect_action' ],
+	      '--action with incorrect value');
+command_fails([ 'pg_checksums', '-A', 'enable', '-A', 'disable' ],
+	      '--action already specified');
+command_fails([ 'pg_checksums', '-D' ], 'no data directory specified');
+my $tempdir = TestLib::tempdir;
+command_fails([ 'pg_checksums', '-D', "$tempdir" ], 'no action specified');
+command_fails([ 'pg_checksums', 'foo', 'bar' ], 'too many arguments');
--- pg_checksums/t/002_actions.pl
+++ pg_checksums/t/002_actions.pl
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use PostgresNode;
 use TestLib;
-use Test::More tests => 12;
+use Test::More tests => 17;
 
 # Run a simple command and grab its stdout output into a result given back
 # to caller.
@@ -43,17 +43,25 @@ mkdir "$pgdata/global/pgsql_tmp";
 append_to_file "$pgdata/global/pgsql_tmp/1.1", "foo";
 
 # Error, server must be shut down cleanly.
-command_fails(['pg_checksums', '--action', 'verify', '-d', $pgdata],
+command_fails(['pg_checksums', '--action', 'verify', '-d', '-D', $pgdata],
 	      "pg_checksums requires server to be cleanly stopped");
 $node->stop;
 
 # Run verification, which should fail as checksums are disabled.
-command_fails(['pg_checksums', '--action', 'verify', '-d', $pgdata],
+command_fails(['pg_checksums', '--action', 'verify', '-d', '-D', $pgdata],
 	      "checksums disabled so no verification");
 
+# Checksums already disabled
+command_fails(['pg_checksums', '--action', 'disable', '-d', '-D', $pgdata],
+	      "checksums already disabled");
+
 # Enable checksums on cluster.
-command_ok(['pg_checksums', '--action', 'enable', '-d', $pgdata],
+command_ok(['pg_checksums', '--action', 'enable', '-d', '-D', $pgdata],
 	   "checksums enabled");
+
+# Checksums already enabled
+command_fails(['pg_checksums', '--action', 'enable', '-d', '-D', $pgdata],
+	      "checksums already enabled");
 
 # Control file should know that checksums are enabled.
 command_like(['pg_controldata', $pgdata],
@@ -61,14 +69,44 @@ command_like(['pg_controldata', $pgdata],
 	     'checksums are enabled in control file');
 
 # This time verification is able to work.
-command_ok(['pg_checksums', '--action', 'verify', '-d', $pgdata],
+command_ok(['pg_checksums', '--action', 'verify', '-d', '-D', $pgdata],
 	   "checksums enabled so verification happens");
 
 # Disable checksums.
-command_ok(['pg_checksums', '--action', 'disable', '-d', $pgdata],
+command_ok(['pg_checksums', '--action', 'disable', '-d', '-D', $pgdata],
 	   "checksums disabled");
 
 # Control file should know that checksums are disabled.
 command_like(['pg_controldata', $pgdata],
 	     qr/Data page checksum version:.*0/,
 	     'checksums are enabled in control file');
+
+# Enable checksums once again before stressing corruption checks.
+run_log(['pg_checksums', '--action', 'enable', '-d', '-D', $pgdata]);
+$node->start;
+
+# Create table to corrupt and get its relfilenode
+$node->safe_psql('postgres',
+	"SELECT a INTO corrupt1 FROM generate_series(1,10000) AS a;
+	ALTER TABLE corrupt1 SET (autovacuum_enabled=false);");
+my $file_corrupted = $node->safe_psql('postgres',
+	"SELECT pg_relation_filepath('corrupt1')");
+my $relfilenode_corrupted =  $node->safe_psql('postgres',
+	"SELECT relfilenode FROM pg_class WHERE relname = 'corrupt1';");
+
+# Set page header and block size
+my $pageheader_size = 24;
+my $block_size = $node->safe_psql('postgres', 'SHOW block_size;');
+$node->stop;
+
+# Time to create some corruption
+open my $file, '+<', "$pgdata/$file_corrupted";
+seek($file, $pageheader_size, 0);
+syswrite($file, '\0\0\0\0\0\0\0\0\0');
+close $file;
+
+$node->command_checks_all([ 'pg_checksums', '-A', 'verify', '-D', $pgdata],
+			  1,
+			  [qr/Bad checksums:.*1/],
+			  [qr/checksum verification failed/],
+			  'fails with corrupted data');
