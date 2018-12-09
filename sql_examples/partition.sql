a44b507399b6486730f53fad0932c9e5fb9ee06a
Michael Paquier <michael@paquier.xyz>
2018-12-09 09:02:56 +0900

    scripts: Add example of multi-layer partition

---
+++ sql_examples/partition.sql
@@ -0,0 +1,20 @@
+-- partitions
+
+-- Template of multi-layer partitions with data.
+CREATE TABLE parent_tab (id int) PARTITION BY RANGE (id);
+CREATE INDEX parent_index ON parent_tab (id);
+CREATE TABLE child_0_10 PARTITION OF parent_tab
+     FOR VALUES FROM (0) TO (10);
+CREATE TABLE child_10_20 PARTITION OF parent_tab
+     FOR VALUES FROM (10) TO (20);
+CREATE TABLE child_20_30 PARTITION OF parent_tab
+     FOR VALUES FROM (20) TO (30);
+INSERT INTO parent_tab VALUES (generate_series(0,29));
+CREATE TABLE child_30_40 PARTITION OF parent_tab
+     FOR VALUES FROM (30) TO (40)
+     PARTITION BY RANGE(id);
+CREATE TABLE child_30_35 PARTITION OF child_30_40
+     FOR VALUES FROM (30) TO (35);
+CREATE TABLE child_35_40 PARTITION OF child_30_40
+     FOR VALUES FROM (35) TO (40);
+INSERT INTO parent_tab VALUES (generate_series(30,39));
