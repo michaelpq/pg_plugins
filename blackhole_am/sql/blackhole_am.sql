03c394ec73d3a354e4ccc96ce8f9649dcb5544a1
Michael Paquier <michael@paquier.xyz>
2021-07-29 15:51:19 +0900

    blackhole_am: Add tests based on ALTER TABLE .. SET ACCESS METHOD
    This subcommand flavor has been added in PostgreSQL 15, with b048326.

--- blackhole_am/sql/blackhole_am.sql
+++ blackhole_am/sql/blackhole_am.sql
@@ -7,3 +7,13 @@ UPDATE blackhole_tab SET a = 0 WHERE a = 1;
 SELECT * FROM blackhole_tab;
 DELETE FROM blackhole_tab WHERE a = 1;
 SELECT * FROM blackhole_tab;
+
+-- ALTER TABLE SET ACCESS METHOD
+ALTER TABLE blackhole_tab SET ACCESS METHOD heap;
+INSERT INTO blackhole_tab VALUES (1);
+SELECT * FROM blackhole_tab;
+ALTER TABLE blackhole_tab SET ACCESS METHOD blackhole_am;
+SELECT * FROM blackhole_tab;
+
+-- Clean up
+DROP TABLE blackhole_tab;
