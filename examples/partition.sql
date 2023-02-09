a42a36f7edce89e2405199815e419856b0416a8b
Michael Paquier <michael@paquier.xyz>
2023-02-09 09:59:36 +0900

    examples: Add SQL example for partitions with lists of values

--- examples/partition.sql
+++ examples/partition.sql
@@ -1,6 +1,6 @@
 -- partitions
 
--- Template of multi-layer partitions with data.
+-- Template of multi-layer partitions with data, with range
 CREATE TABLE parent_tab (id int) PARTITION BY RANGE (id);
 CREATE INDEX parent_index ON parent_tab (id);
 CREATE TABLE child_0_10 PARTITION OF parent_tab
@@ -18,3 +18,9 @@ CREATE TABLE child_30_35 PARTITION OF child_30_40
 CREATE TABLE child_35_40 PARTITION OF child_30_40
      FOR VALUES FROM (35) TO (40);
 INSERT INTO parent_tab VALUES (generate_series(30,39));
+
+-- Partitions with lists of values
+CREATE TABLE parent_list (id int) PARTITION BY LIST (id);
+CREATE TABLE child_list PARTITION OF parent_list
+     FOR VALUES IN (1, 2);
+INSERT INTO parent_list VALUES (1), (2);
