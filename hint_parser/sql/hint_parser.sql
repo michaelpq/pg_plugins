60c2462709cb852ffd938c45f603c2184c87ac25
Michael Paquier <michael@paquier.xyz>
2023-04-23 11:23:38 +0900

    hint_parser: Add test with newline patterns
    All these are authorized by the parser.

--- hint_parser/sql/hint_parser.sql
+++ hint_parser/sql/hint_parser.sql
@@ -4,6 +4,15 @@ SELECT * FROM hint_parser('i(i)');
 SELECT * FROM hint_parser('hint1(content1)hint2(content2)');
 SELECT * FROM hint_parser('hint1( content1 ) hint2  ( content2 )');
 SELECT * FROM hint_parser('  hint1 ( content1 ) h2 (c2) h3 (c3 )');
+-- Valid, with newlines
+SELECT * FROM hint_parser('
+h1
+  (
+  c1
+  )
+h2(c2) h3(c3)
+  h4(c4)
+');
 -- Broken hints
 SELECT hint_parser('');
 SELECT hint_parser('I');
