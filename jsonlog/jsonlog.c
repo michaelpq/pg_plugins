michaelpq/pg_plugins

commit 56ba5c818f8464c7b62b1499701f1e1a3bce5a2a

jsonlog: Fix compilation with latest PostgreSQL version

jsonlog/jsonlog.c
@@ -378,9 +378,9 @@ jsonlog_write_json(ErrorData *edata)
 
 	/* Virtual transaction id */
 	/* keep VXID format in sync with lockfuncs.c */
-	if (MyProc != NULL && MyProc->backendId != InvalidBackendId)
+	if (MyProc != NULL && MyProc->vxid.procNumber != INVALID_PROC_NUMBER)
 		appendStringInfo(&buf, "\"vxid\":\"%d/%u\",",
-						 MyProc->backendId, MyProc->lxid);
+						 MyProc->vxid.procNumber, MyProc->vxid.lxid);
 
 	/* Transaction id */
 	if (txid != InvalidTransactionId)

commit d101c4f6995de02bd11045c66ad6651675d0d4eb

jsonlog: Fix compilation with versions of PostgreSQL < 17
This has been broken in 56ba5c818f84, but like for 2afb1b599a84 folks
seem to care about compatibility.

jsonlog/jsonlog.c
@@ -378,9 +378,15 @@ jsonlog_write_json(ErrorData *edata)
 
 	/* Virtual transaction id */
 	/* keep VXID format in sync with lockfuncs.c */
+#if PG_VERSION_NUM >= 170000
 	if (MyProc != NULL && MyProc->vxid.procNumber != INVALID_PROC_NUMBER)
 		appendStringInfo(&buf, "\"vxid\":\"%d/%u\",",
 						 MyProc->vxid.procNumber, MyProc->vxid.lxid);
+#else
+	if (MyProc != NULL && MyProc->backendId != InvalidBackendId)
+		appendStringInfo(&buf, "\"vxid\":\"%d/%u\",",
+						 MyProc->backendId, MyProc->lxid);
+#endif
 
 	/* Transaction id */
 	if (txid != InvalidTransactionId)